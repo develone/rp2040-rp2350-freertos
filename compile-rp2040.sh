9cbe30966e188cdc48c58961660d80399267e132
Edward Vidal Jr <develone@sbcglobal.net>
2025-01-15 20:14:28 -0700

    adding missing

--- compile-rp2040.sh
+++ compile-rp2040.sh
@@ -2,6 +2,6 @@
 rm -rf build-2040 
 mkdir build-2040
 cd build-2040
-cmake -DPICO_BOARD=pico ..
+cmake .. -DPICO_BOARD=pico -DPICO_PLATFORM=rp2040 
 make
 
--- compile-rp2350.sh
+++ compile-rp2350.sh
@@ -2,6 +2,6 @@
 rm -rf build-2350 
 mkdir build-2350
 cd build-2350
-cmake -DPICO_BOARD=pico2 ..
+cmake .. -DPICO_BOARD=pico2 -DPICO_PLATFORM=rp2350 
 make
 
