8656feec5d6fb4bf628ade4c63c755545b401609
Edward Vidal Jr <develone@sbcglobal.net>
2025-01-17 18:49:31 -0700

    adding proto type

--- openjpeg/openjpeg-lib/dwtlift.h
+++ openjpeg/openjpeg-lib/dwtlift.h
@@ -1,3 +1,5 @@
+#ifndef	DWTLIFT_H
+#define	DWTLIFT_H
 
 double memory;
  
@@ -6,10 +8,43 @@ struct timeval currentTime,start,end;
  
 long mtime, seconds, useconds;
 
-double sqrt(double x);
+int da_x0, int da_y0, int da_x1, int da_y1, const char *input_file;
+
+int x0, int y0, int x1, int y1, char *ff_in;
+
+extern double sqrt(double x);
  
-void lift_config(int dec, int enc, int TCP_DISTORATIO, int FILTER,  int CR, int flg, int bp, long imgsz,long him,long wim, int *bufferptr);
+extern void lift_config(int dec, int enc, int TCP_DISTORATIO, int FILTER,  int CR, int flg, int bp, long imgsz,long him,long wim, int *bufferptr);
 
  
 
 typedef int int32;
+
+extern RGB** createMatrix();
+
+extern RGB** loadImage(FILE *arq, RGB** Matrix);
+
+extern void Wr_p_matrix(RGB** Matrix,char *r,char *g,char *b);
+
+extern void Wr_pp_matrix(RGB** Matrix,char *r,char *g,char *b);
+
+extern void p_matrix(RGB** Matrix,char *r,char *g,char *b);
+
+extern void pp_matrix(RGB** Matrix,char *r,char *g,char *b);
+
+extern INFOHEADER readInfo(FILE *arq);
+
+extern void writeImage(FILE *arqw, RGB** Matrix);
+
+void writeInfo(FILE *arqw,INFOHEADER infowrite);
+
+void isBMP(FILE* arq, INFOHEADER info);
+
+extern static int infile_format(const char *fname);
+
+extern int get_file_format(const char *filename);
+
+extern int decompress(int da_x0, int da_y0, int da_x1, int da_y1, const char *input_file);
+extern void decom_test(int x0, int y0, int x1, int y1, char *ff_in);
+
+#endif
